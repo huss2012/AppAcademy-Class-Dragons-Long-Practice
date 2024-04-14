baaf0c6c9e94d833915df9ac622cc111593af1e7
huss2012 <huss24940@gmail.com>
2024-04-14 08:18:44 +0300

    Phase:1 Create a Dragon has been solved.

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,5 +1,41 @@
 // Your code here
 
+const { newFixFrameDecoder } = require("fix/handlers/fixFrameDecoder");
+
+
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+
+  static getDragons(...dragons) {
+    // let resArr = [];
+    // for (let i = 0; i < dragons.length; i++){
+    //   resArr.push(dragons[i].name);
+    // }
+    // return resArr;
+
+    return dragons.map(dragon => {
+      return dragon.name;
+    })
+  }
+}
+
+//--------------Local test-----------------------------
+const puff = new Dragon("Puff", "green");
+console.log(puff);
+console.log(puff.breathesFire());
+
+const toothless = new Dragon("Toothless", "Black");
+console.log(toothless);
+console.log(toothless.breathesFire());
+
+console.log(Dragon.getDragons(puff, toothless));
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
