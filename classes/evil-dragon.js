acdfb05fab799d72fed2616c35b70d5d9367f374
huss2012 <huss24940@gmail.com>
2024-04-14 08:56:26 +0300

    Phase 3: Create Evil Dragon has been solved.

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,1 +1,62 @@
 // Your code here
+
+const Dragon = require("./dragon");
+
+class EvilDragon extends Dragon{
+    constructor(name, color, evilDoings, nemesis) {
+        super(name, color, evilDoings, nemesis);
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
+    }
+
+    dontInviteThemOverForDinner() {
+        this.evilDoings.forEach(evileDoing => {
+            console.log(`${this.name} will ${evileDoing}`);
+        });
+    };
+
+    burnsNemesis() {
+        return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`;
+    }
+}
+
+
+
+
+
+
+//---------Local Test-----------------------
+const enderDragon = new EvilDragon(
+    "Ender Dragon",
+    "black", [
+    "spit a fireball at Steve",
+    "rule over all the Endermen",
+    "destroy all blocks in its way"
+],
+    "Steve"
+);
+// console.log(enderDragon);
+// enderDragon.dontInviteThemOverForDinner();
+// console.log(enderDragon.breathesFire());
+// console.log(enderDragon.burnsNemesis());
+// const drogon = new EvilDragon(
+//     "Drogon",
+//     "black-red",
+//     [
+//         "eat all your livestock",
+//         "burn down your castle in Westeros",
+//         "burn your father and brother"
+//     ],
+//     "Night King"
+// );
+// console.log(drogon);
+// drogon.dontInviteThemOverForDinner();
+// console.log(drogon.breathesFire());
+// console.log(drogon.burnsNemesis());
+
+//------------Exporting the module----------
+try {
+    module.exports = EvilDragon;
+} catch {
+    module.exports = null;
+}
